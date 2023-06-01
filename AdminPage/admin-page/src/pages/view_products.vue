38e49f1bc3159ab31c078ef19862583637d337c6
ConorCallanan2003 <conorcallanan03@gmail.com>
2023-06-01 10:39:46 +0100

    view products page calls api - cors needs to be enabled though

--- AdminPage/admin-page/src/pages/view_products.vue
+++ AdminPage/admin-page/src/pages/view_products.vue
@@ -3,12 +3,8 @@
     <main class="container">
         <h1 style="margin-bottom: 10px !important; ">View Products</h1> 
       <div>
-        <div class="grid">
-          <ItemComp name="iPhone" price="100" description="Great product" image="🍾"></ItemComp>
-          <ItemComp name="iPhone" price="100" description="Great product" image="🍾"></ItemComp>
-          <ItemComp name="iPhone" price="100" description="Great product" image="🍾"></ItemComp>
-          <ItemComp name="iPhone" price="100" description="Great product" image="🍾"></ItemComp>
-          <ItemComp name="iPhone" price="100" description="Great product" image="🍾"></ItemComp>
+        <div class="grid"> 
+          <ItemComp v-for="(item, index) in responseData" :key="index" :name="item.name" :price="item.price" :description="item.description" :image="item.image"></ItemComp>
         </div>
       </div>
     </main>
@@ -22,6 +18,25 @@
 import ItemComp from '../components/ItemComp.vue';
 export default {
   name: "ViewProductsPage",
+  data() {
+    return {
+      responseData: null
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch('http://localhost:2222/products')
+        .then(response => {
+          this.responseData = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  mounted() {
+    this.fetchData();
+  },
   components: {
     ItemComp
   },
